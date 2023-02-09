4b47614cb49013b1528e0f6ab3fd5b256ffa19f0
Carlos Moraes <cmoraes@unifesp.br>
2023-02-09 13:16:53 -0300

    Update gitignore

--- api/controller/app.py
+++ api/controller/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from service.modeling import Modeling
 from flask_cors import CORS
 from flask_cors import cross_origin
+import json
 
 app = Flask(__name__)
 ucs = Modeling()
@@ -11,13 +12,13 @@ class Grade():
 
     @app.route("/disciplinas", methods=['GET'])
     def get():
-        return ucs.get_ucs()
+        return json.dumps(ucs.get_ucs())
 
     @app.route("/disciplinas", methods=['POST'])
     def post():
         data = request.get_json()
         data = data['items']
-        return ucs.uc_analizer(data)
+        return json.dumps(ucs.uc_analizer(data))
 
     def start(debug=False):
         return app.run(debug=debug)
