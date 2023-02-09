55f750675d4006c6453fc371795c156219cde537
Victor Pedrota <58073023+vpedrota@users.noreply.github.com>
2023-02-09 17:25:00 -0300

    Update app.py

--- api/controller/app.py
+++ api/controller/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from service.modeling import Modeling
 from flask_cors import CORS
 from flask_cors import cross_origin
+import os
 
 app = Flask(__name__)
 ucs = Modeling()
@@ -19,5 +20,5 @@ class Grade():
         data = data['items']
         return ucs.uc_analizer(data)
 
-    def start(debug=False):
-        return app.run(debug=debug)
+    def start(debug=False, host="0.0.0.0", port="8080"):
+        return app.run(debug=debug, host="host", port=int(os.environ.get("PORT", port))) 
