c05a908caee2ca28646d1cac38c59d082cb05a27
estevanatto <felipelimastevanatto@gmail.com>
2023-08-03 18:46:28 -0300

    feat: tailwind implementado estavelmente

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -1,15 +1,12 @@
 <template>
-  <div class="">
-    <div class="text-base">
-      <input
-        type="text"
-        v-model="query"
-        class="shadow-md rounded-md px-3 py-2 w-full"
-        :placeholder="placeholder"
-        @input="$emit('search', query)"
-      >
-    </div>
-    <slot></slot>
+  <div class="text-base">
+    <input
+      type="text"
+      v-model="query"
+      class="shadow-md rounded-md px-3 py-2 w-full"
+      :placeholder="placeholder"
+      @input="$emit('search', query)"
+    >
   </div>
 </template>
 
