3705110b949138ea2b90c29308f68f0104615032
g-libardi <guilhermelibardi@hotmail.com>
2024-09-29 22:16:33 -0400

    feat: utilizar novo composable no botão do google

--- frontend-montador/src/components/GoogleLoginButton.vue
+++ frontend-montador/src/components/GoogleLoginButton.vue
@@ -1,37 +1,20 @@
 <template>
     <button class="bg-sky-700 p-3 rounded-xl text-black font-semibold"
-        @click="googleSignIn">
+        @click="signIn">
         Sign In with Google
     </button>
 </template>
 
 <script>
 import CustomButton from './CustomButton.vue';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-
-
-function googleSignIn() {
-    const auth = getAuth();
-    let provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider)
-        .then((result) => {
-          //fetch data from here to see if the user is loged
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            // The signed-in user info.
-            const user = result.user;
-            console.log(token) // Token
-            console.log(user) // User that was authenticated
-        })
-        .catch((err) => {
-            console.error(err); // This will give you all the information needed to further debug any errors
-        });
-}
+import useAuth from '../composables/useAuth';
 
 export default {
     methods: {
-        googleSignIn
+        signIn () {
+            const { signInWithGoogle } = useAuth();
+            signInWithGoogle();
+        }
     }
 }
 </script>
